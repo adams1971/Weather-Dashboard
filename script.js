04efbd7ebbc654d4bf933957aa0f65902cdc2219
adam stevenson <adamscdcm@gmail.com>
2021-02-05 18:55:49 -0800

    worked city recall

--- script.js
+++ script.js
@@ -1,55 +1,107 @@
-//GIVEN a weather dashboard with form inputs
-//WHEN I search for a city
-//1 get API and key
-
+//may need var for lat and lon
 $(document).ready(function () {
+  var currentSearch = JSON.parse(localStorage.getItem("searchHistory")) || [];
+  var apiKey = "75dd5d0d667ecff1fa08e5eb255eef0b";
+
+
+  var d = new Date();
+  document.getElementById("date").innerHTML = d;
+
+  if (currentSearch.length != 0) {
+    rendorPastSearches();
+    callWeatherInfo(0);
+  }
+
+  //call to search entered city
+  $("#searchCityBtn").on("click", function () {
     var farenheit = "&units=imperial";
     var appId = "&appid=";
-    var apiKey = "75dd5d0d667ecff1fa08e5eb255eef0b";
-    
-    $("#searchCityBtn").on("click", function () {
-        var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=";
-        
-        var cityEntered = $("#cityEntered").val();
-        var ajaxUrl = weatherURL+cityEntered+farenheit+appId+apiKey;
-        // console.log("city=" + cityEntered);
-        // console.log("url: " + ajaxUrl);
-    
-        $.ajax({
-            url: ajaxUrl,
-            method: "GET",
-        }).then(function (response) {
-            console.log(response);
-            $("#cityName").text(response.name);
-            $("#currentTemp").text(response.main.temp)
-            $("#currentHum").text(response.main.humidity)
-            $("#currentWind").text(response.wind.speed)
-        });
- 
-        var weatherUVI = "";//remember to add the api url
 
-        var ajaxUVI = weatherUVI+cityEntered+appId+apiKey;
+    //weather only API
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+    var cityEntered = $("#cityEntered").val();
 
+    var selectedUrl = weatherURL + cityEntered + farenheit + appId + apiKey;
+
+    $.ajax({
+      url: selectedUrl,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      $("#cityName").text(response.name);
+      $("#currentTemp").append(response.main.temp);
+      $("#currentHum").append(response.main.humidity);
+      $("#currentWind").append(response.wind.speed);
+
+      var longitude = response.coord.lon;
+      var latitude = response.coord.lat;
+
+      var uvURL =
+        "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+        latitude +
+        "&lon=" +
+        longitude +
+        appId +
+        apiKey;
+
+      $.ajax({
+        url: uvURL,
+        method: "GET",
+      }).then(function (res) {
+        console.log("res=", res);
+        var uvi = res.current.uvi;
+        console.log("uvi = " + uvi);
+        $("#currentUVI").append(uvi);
+      });
     });
+  });
 
+  $(document).on("click", ".weatherSearch", function () {
+    var index = this.value;
+    callWeatherInfo(index);
+    rendorPastSearches(index);
+  });
 
+  function rendorPastSearches() {
+    $("#Past-Searches").empty();
+    for (var i = 0; i < currentSearch.length; i++) {
+      var location = currentSearch[i].location;
 
+      var $li = $("<li>");
+      $li.className = "list-group-item";
+      var btn = $("<button>").text(location);
+      $li.append(btn);
+      btn.attr({
+        class: "btn btn-light weatherSearch",
+        value: i,
+      });
 
+      $("#Past-Searches").append($li);
+    }
+  }
 
+  function callWeatherInfo(index) {
+    var temp = currentSearch[index].currentObjectWeather;
+    //$("#cityName").text(temp.name);
+    //keep appending values
+  }
 
+  function updateWeather(location, currentObjectWeather, forecast, uviIndex) {
+    var tempObj = {
+      location: "",
+      time: moment(),
+      currentObjectWeather: {},
+      forecast: {},
+      uviIndex: {},
+    };
+
+    tempObj.location = location;
+    tempObj.currentObjectWeather = currentObjectWeather;
+    tempObj.forecast = forecast;
+    tempObj.uviIndex= uviIndex;
+
+    currentSearch.push(tempObj);
+    localStorage.setItem("searchHistory", JSON.stringify(currentSearch));
+  }
+  //call render past searches with local storage hist
 });
-//THEN I am presented with current and future conditions for that city and that city is added to the search history
-//2 add the search to local storage
-//WHEN I view current weather conditions for that city
-//THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-//3 add mon=ment.js for date
-//4 use font aswesome and show icon
-//WHEN I view the UV index
-//THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-//5 use a new API for UVI
-//WHEN I view future weather conditions for that city
-//THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-//6 dynamically add the 5 day forcast cards
-//WHEN I click on a city in the search history
-//THEN I am again presented with current and future conditions for that city
-//7 repeat
